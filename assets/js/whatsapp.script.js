a437d41cfde7e4cedc3a64785f8b3f2223c2b532
Josue <joshvlbn@gmail.com>
2025-09-28 16:52:34 -0300

    avance

---
+++ assets/js/whatsapp.script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const whatsappButtonBeginner = document.getElementById('btn-course-beginner');
+    const whatsappButtonIntermediate = document.getElementById('btn-course-intermediate');
+    const whatsappButtonAdvance = document.getElementById('btn-course-advance');
+
+    const sendWhatsapp = () => {
+        const nivel = document.getElementById('title-course-beginner');
+        const mensajeOriginal = `Hola, estoy interesado(a) en el curso de ${nivel}.
+Me gustaría saber cómo puedo inscribirme o si tienen alguna promoción.`;
+        const mensajeCodificado = encodeURIComponent(mensajeOriginal);
+        const telefono = '+56954972720';
+        const whatsappUrl = `https://wa.me/${telefono}?text=${mensajeCodificado}`;
+        window.open(whatsappUrl, '_blank');
+    };
+
+    if (whatsappButtonBeginner) {
+        whatsappButtonBeginner.addEventListener('click', sendWhatsapp);
+    }
+});
