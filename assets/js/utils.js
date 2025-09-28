0b74a708ac7768de056ac2a0dd119bc8945ff20d
Josue <joshvlbn@gmail.com>
2025-09-28 18:06:50 -0300

    avance

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,6 +1,6 @@
 export const sendWhatsapp = (mensaje) => {
     const mensajeCodificado = encodeURIComponent(mensaje);
-    const telefono = '+56954972720';
+    const telefono = '+5219331375679';
     const whatsappUrl = `https://wa.me/${telefono}?text=${mensajeCodificado}`;
     window.open(whatsappUrl, '_blank');
 };
